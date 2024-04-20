3dbc0297489db58e72bd984fb6e42ecc3693749b
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-20 11:51:17 +0300

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/bin/bash
-# Install flask
-
-package {    'flask':
-    ensure   => '2.1.0',
-    provider => 'pip',
+#1-insall_a_package.pp
+package{      'flask':
+  ensure   => installed,
+  provider => 'pip3',
 }
