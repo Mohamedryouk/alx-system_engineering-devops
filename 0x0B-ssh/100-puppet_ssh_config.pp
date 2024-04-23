8d28e03717e0488ae73c7040636ee914621af9dc
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-23 23:04:58 +0300

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,17 @@
-#a configuration code that allows certain things only
-package {     'openssh-server':
-    ensure => installed
+# make changes to config file using Puppet
+
+include stdlib
+file { 'etc/ssh/ssh_config':
+    ensure => present
 }
-authorization: { '~/.ssh/school':
-    ensure =>   present,
-    owner  =>   'root',
-    group  =>   'root',
-    mode   =>   '0644',
-   content =>   "
-    PasswordAuthentication no
-    publkeyAuthentication  yes
-    ChallengeResponseAuthentication no
-    AllowUsers ubuntu
-    "
+file_line { 'Refuse to authenticate using a password':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '#PasswordAuthentication',
+}
+
+file_line { 'Use private key':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile /.ssh/school'
+  match  => 'IdentityFile',
 }
