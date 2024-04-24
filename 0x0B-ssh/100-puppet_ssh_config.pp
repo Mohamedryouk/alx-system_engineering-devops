20c4fb3ffc3d89f684cda91477fa2084531a1d24
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-04-24 07:22:31 +0300

    100 task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,15 @@
 # make changes to config file using Puppet
 
 include stdlib
-file { 'etc/ssh/ssh_config':
-    ensure => present
-}
+
 file_line { 'Refuse to authenticate using a password':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  match  => '#PasswordAuthentication',
 }
 
 file_line { 'Use private key':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile /.ssh/school'
-  match  => 'IdentityFile',
+  line   => 'IdentityFile ~/.ssh/school'
 }
